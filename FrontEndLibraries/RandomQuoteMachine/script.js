eb5767c5853d678f28dd74d1f62b7a27bc9f3756
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-09-15 17:21:50 -0500

    simplify js script

--- FrontEndLibraries/RandomQuoteMachine/script.js
+++ FrontEndLibraries/RandomQuoteMachine/script.js
@@ -1,32 +1,19 @@
 const projectName = "random-quote-machine";
-var currentQuote = 'Hammer Hammer Hammer', currentAuthor = 'John Henry';
-
-
+var tweetEndpoint = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="
 
 function getQuote() {
-    console.log('getQuote()');
-    $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-
     $.getJSON('data.json', function(json) {
         randomQuote = json.quotes[Math.floor(Math.random() * json.quotes.length)];
         quote = randomQuote.quote;
         author = randomQuote.author;
         $('#text').text(quote);
         $('#author').text(author);
+        $('#tweet-quote').attr('href', tweetEndpoint + encodeURIComponent('"' + quote + '" --' + author));
     });
 }
 
-
-
 $(document).ready(function() {
-    console.log('$(document).ready(function()');
-    $('#text').text(currentQuote);
-    $('#author').text(currentAuthor);
-
+    getQuote();
     $('#new-quote').on('click', getQuote);
-
-    $('#tweet-quote').on('click', function() {
-        openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-      });
 });
 
