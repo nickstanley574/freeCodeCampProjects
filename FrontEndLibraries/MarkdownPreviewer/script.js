30435dd4b78fa4cfcde5478b7424a65d863d133f
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-09-26 21:38:14 -0500

    added external scripts and helloworld script

--- FrontEndLibraries/MarkdownPreviewer/script.js
+++ FrontEndLibraries/MarkdownPreviewer/script.js
@@ -1,10 +1,28 @@
+const projectName = "markdown-previewer";
+
+// ALLOWS LINE BREAKS WITH RETURN BUTTON
+marked.setOptions({
+    breaks: true,
+  });
+
+console.log(marked('[link](www)'))
+
+const placeholder = 
+`
+# Place Holder Markdown 
+### Hello World
+`
+
 class App extends React.Component{
     constructor(props) {
       super(props);
+      this.state = {
+          markdown: placeholder
+      }
     }
     render() {
         return (
-            <h3>Hello World!</h3>
+            <h3>Markdown Previewer</h3>
         )
     }
 }
