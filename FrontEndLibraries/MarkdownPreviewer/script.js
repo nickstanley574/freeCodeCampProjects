6ed6151a0fd945ab031d16677d7cfe7bad73d6af
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-10-01 15:59:43 -0500

    added Toolbar, Editor, and Preview const plus css changes

--- FrontEndLibraries/MarkdownPreviewer/script.js
+++ FrontEndLibraries/MarkdownPreviewer/script.js
@@ -7,6 +7,36 @@ marked.setOptions({
 
 console.log(marked('[link](www)'))
 
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+  return `<a target="_blank" href="${href}">${text}` + '</a>';
+}
+
+const Toolbar = (props) => {
+    return (
+      <div className="toolbar">
+        <i title="no-stack-dub-sack" className="fa fa-free-code-camp"/>      
+        {props.text}
+        <i onClick={props.onClick} className={props.icon}></i>
+      </div>
+   )
+}
+
+const Editor = (props) => {
+  return (
+    <textarea id="editor"
+      value={props.markdown}
+      onChange={props.onChange}
+      type="text"/>
+    )
+}
+const Preview = (props) => {
+  return (
+    <div id='preview' dangerouslySetInnerHTML={{__html: marked(props.markdown, {renderer: renderer})}}/>
+  )
+}
+
+
 const placeholder = 
 `
 # Place Holder Markdown 
@@ -19,10 +49,26 @@ class App extends React.Component{
       this.state = {
           markdown: placeholder
       }
+      this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e) {
+      this.setState({
+        markdown: e.target.value
+      });
     }
     render() {
         return (
-            <h3>Markdown Previewer</h3>
+            <div>
+                <h3>Markdown Previewer</h3>
+                <div className='editorWrap'>
+                    <Toolbar text="Editor"/>
+                    <Editor markdown={this.state.markdown}/>  
+                </div>
+                <div className='previewWrap'>
+                  <Toolbar text="Previewer"/>
+                  <Preview markdown={this.state.markdown}/> 
+                </div>
+            </div>
         )
     }
 }
