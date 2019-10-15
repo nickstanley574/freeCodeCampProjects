76b5b741d8f68c25b60e48ce555fab2d30a86a78
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-10-15 17:22:43 -0500

    init commit of calculator files

---
+++ FrontEndLibraries/JavaScriptCalculator/script.js
@@ -0,0 +1,12 @@
+
+const projectName = 'javascript-calculator';
+
+class App extends React.Component {
+    render () {
+        return (
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+ReactDOM.render(React.createElement(App, null), document.getElementById("app"));
