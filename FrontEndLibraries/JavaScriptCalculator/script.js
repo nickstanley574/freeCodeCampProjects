72b54167a6da1707410dcc315de3b358b5a33974
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2019-10-17 08:58:29 -0500

    bare bones calc all tests past

--- FrontEndLibraries/JavaScriptCalculator/script.js
+++ FrontEndLibraries/JavaScriptCalculator/script.js
@@ -1,82 +1,128 @@
 
 const projectName = 'javascript-calculator';
 
+const   endsWithNegativeSign =/‑$/,
+        isOperator = /[*/+-]/
+
+
+
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             display: 0,
             prevDisplay: 0,
-            hasBeenClicked: false
+            hasBeenClicked: false,
+            decimalAllowed: true,
+            prevWasOperator: false,
         };
-        this.displayCallback = this.displayCallback.bind(this);
     }
 
 
-    displayCallback = (e) => {
-        console.log(e.target)
 
-        if (this.state.display == '0'){
-            var result = e.target.value
-        } else {
-            var result = this.state.display + e.target.value;
+    displayOperator = (e) => {
+        var result = this.state.display
+
+        console.log(result)
+        console.log(/^(.*?)[*/+-][*/+-]$/.test(result))
+
+        if (/^(.*?)[*/+-][*/+-]$/.test(result)) {
+            result = result.substring(0, result.length - 2);
+        }
+        else if (this.state.prevWasOperator && e.target.value != "-") {
+            result = result.substring(0, result.length - 1);
+        }
+
+        if (!result.endsWith('-')) {
+            result = result + e.target.value;
         }
 
         this.setState({
-            display: result
+            display: result,
+            decimalAllowed: true,
+            prevWasOperator: true
         })
     }
+
+
+
+    displayNumber = (e) => {
+        var result = this.state.display
+
+        if (this.state.display == '0') {
+            result = e.target.value
+        } else {
+            result = result + e.target.value;
+        }
+
+        this.setState({
+            display: result,
+            prevWasOperator: false
+        })
+    }
+
+
+
+    displayDecimal = (e) => {
+        var result = this.state.display
+
+        if (this.state.decimalAllowed) {
+            var result = this.state.display + "."
+        }
+
+        this.setState({
+            display: result,
+            decimalAllowed: false
+        })
+    }
+
+
 
     displayClear = () => {
         this.setState({
-            display: 0
+            display: 0,
+            decimalAllowed: true,
+            prevWasOperator: false
         })
     }
+
+
 
     evaluateExpression = () => {
-        var result = eval(this.state.display);
-        console.log(result);
+        var result = eval(this.state.display).toString();
         this.setState({
-            display: result
+            display: result,
         })
     }
 
-    handleClick = () => {
-        this.setState({
-          hasBeenClicked: true
-        })
-    }
+
 
     render () {
         return (
-            // <h1>Hello World</h1>
             <div>
                 <div id="display">
                     {this.state.display}
                 </div>
                 <button id="clear" onClick={this.displayClear}>AC</button>
 
-                <button id="zero"  value='0'  onClick={e => this.displayCallback(e, "value")}>0</button>
-                <button id="one"   value='1'  onClick={e => this.displayCallback(e, "value")}>1</button>
-                <button id="two"   value='2'  onClick={e => this.displayCallback(e, "value")}>2</button>
-                <button id="three" value='3'  onClick={e => this.displayCallback(e, "value")}>3</button>
-                <button id="four"  value='4'  onClick={e => this.displayCallback(e, "value")}>4</button>
-                <button id="five"  value='5'  onClick={e => this.displayCallback(e, "value")}>5</button>
-                <button id="six"   value='6'  onClick={e => this.displayCallback(e, "value")}>6</button>
-                <button id="seven" value='7'  onClick={e => this.displayCallback(e, "value")}>7</button>
-                <button id="eight" value='8'  onClick={e => this.displayCallback(e, "value")}>8</button>
-                <button id="nine"  value='9'  onClick={e => this.displayCallback(e, "value")}>9</button>
+                <button id="zero"  value='0' onClick={e => this.displayNumber(e, "value")}>0</button>
+                <button id="one"   value='1' onClick={e => this.displayNumber(e, "value")}>1</button>
+                <button id="two"   value='2' onClick={e => this.displayNumber(e, "value")}>2</button>
+                <button id="three" value='3' onClick={e => this.displayNumber(e, "value")}>3</button>
+                <button id="four"  value='4' onClick={e => this.displayNumber(e, "value")}>4</button>
+                <button id="five"  value='5' onClick={e => this.displayNumber(e, "value")}>5</button>
+                <button id="six"   value='6' onClick={e => this.displayNumber(e, "value")}>6</button>
+                <button id="seven" value='7' onClick={e => this.displayNumber(e, "value")}>7</button>
+                <button id="eight" value='8' onClick={e => this.displayNumber(e, "value")}>8</button>
+                <button id="nine"  value='9' onClick={e => this.displayNumber(e, "value")}>9</button>
 
-                <button id="add"      value='+'  onClick={e => this.displayCallback(e, "value")}>+</button>
-                <button id="subtract" value='-'  onClick={e => this.displayCallback(e, "value")}>-</button>
-                <button id="multiply" value='*'  onClick={e => this.displayCallback(e, "value")}>*</button>
-                <button id="divide"   value='/'  onClick={e => this.displayCallback(e, "value")}>/</button>
-                <button id="decimal"  value='.'  onClick={e => this.displayCallback(e, "value")}>.</button>
+                <button id="add"      value='+' onClick={e => this.displayOperator(e, "value")}>+</button>
+                <button id="subtract" value='-' onClick={e => this.displayOperator(e, "value")}>-</button>
+                <button id="multiply" value='*' onClick={e => this.displayOperator(e, "value")}>*</button>
+                <button id="divide"   value='/' onClick={e => this.displayOperator(e, "value")}>/</button>
 
+                <button id="decimal"  onClick={this.displayDecimal}>.</button>
                 <button id="equals"   onClick={this.evaluateExpression}>=</button>
-
-                <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
-                <button onClick={this.handleClick}>Click me!</button>
             </div>
         );
     }
