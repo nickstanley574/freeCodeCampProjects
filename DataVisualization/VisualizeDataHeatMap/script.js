4feff204f7474973f573c74356aefc94591b16e5
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2020-06-04 19:58:07 -0500

    make the legend of the heat map look better

--- DataVisualization/VisualizeDataHeatMap/script.js
+++ DataVisualization/VisualizeDataHeatMap/script.js
@@ -1,9 +1,9 @@
 // Base off: https://www.d3-graph-gallery.com/graph/heatmap_basic.html
 
 // set the dimensions and margins of the graph
-var margin = { top: 30, right: 30, bottom: 30, left: 80 },
-    width = 1300 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin = { top: 80, right: 80, bottom: 150, left: 80 },
+    width = 1250 - margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select(".visHolder")
@@ -25,37 +25,19 @@ url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
 //Read the data
 d3.json(url, function(data) {
 
-    var monthlyVariance = data.monthlyVariance
-
     var yearsArr = []
     var VariArr = []
 
-    monthlyVariance.forEach(element => {
-        yearsArr.push(element['year'])
-        VariArr.push(element['variance'])
-    });
-
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    data.monthlyVariance.map((e, i) => {
+    data.monthlyVariance.map((e) => {
         e.temperature = Math.round((data.baseTemperature + e.variance) * 100) / 100
         e.monthString = months[e.month - 1]
+        yearsArr.push(e.year)
+        VariArr.push(e.variance)
     })
 
-    // Build color scale
-    var colors = [
-        "#313695",
-        "#4575B4",
-        "#74ADD1",
-        "#ABD9E9",
-        "#E0F3F8",
-        "#FFFFBF",
-        "#FEE090",
-        "#FDAE61",
-        "#F46D43",
-        "#D73027",
-        "#A50026"
-    ]
+    var colors = ["#313695", "#4575B4", "#74ADD1", "#ABD9E9", "#E0F3F8", "#FFFFBF", "#FEE090", "#FDAE61", "#F46D43", "#D73027", "#A50026"]
 
     var colorScale = d3.scaleQuantile()
         .domain([d3.min(VariArr), d3.max(VariArr)])
@@ -66,7 +48,6 @@ d3.json(url, function(data) {
     var y = d3.scaleBand()
         .range([height, 0])
         .domain([12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
-        .padding(0.01);
 
     svg.append("g")
         .call(d3.axisLeft(y)
@@ -80,7 +61,6 @@ d3.json(url, function(data) {
     var x = d3.scaleBand()
         .range([0, width])
         .domain(yearsArr)
-        .padding(0.04);
     svg.append("g")
         .attr("id", "x-axis")
         .attr("transform", "translate(0," + height + ")")
@@ -89,7 +69,7 @@ d3.json(url, function(data) {
         )
 
     svg.selectAll()
-        .data(monthlyVariance, function(d) { return d.year + ':' + d.month; })
+        .data(data.monthlyVariance, function(d) { return d.year + ':' + d.month; })
         .enter()
         .append("rect")
         .attr('class', 'cell')
@@ -99,25 +79,25 @@ d3.json(url, function(data) {
         .attr('data-temp', (d, i) => d.variance)
         .attr("x", function(d) { return x(d.year) })
         .attr("y", function(d) { return y(d.month) })
-        .attr("width", x.bandwidth())
-        .attr("height", y.bandwidth())
+        .attr("width", x.bandwidth() + 0.2)
+        .attr("height", y.bandwidth() + 0.1)
         .style("fill", function(d, i) {
             return colorScale(d.variance)
         })
         .on("mouseover", function(d) {
-            d3.select(this).style("stroke", "black")
-            d3.select(this).style("stroke-width", 2.2) // set the str
+            d3.select(this).style("stroke", "gray")
+            d3.select(this).style("stroke-width", 2)
             div.transition()
                 .duration(200)
                 .style("opacity", 1)
                 .attr("data-year", d.year)
 
-            div.html(+d.year + " - " + months[d.month - 1] +
+            div.html(d.year + " - " + months[d.month - 1] +
                     "<br/>" + "Variance: " + d.variance +
-                    "<br/>" + "Temp: " + d.temperature
+                    "<br/>" + "Temp: " + d.temperature + "℃"
                 )
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
+                .style("left", (d3.event.pageX - 50) + "px")
+                .style("top", (d3.event.pageY - 90) + "px");
         })
         .on("mouseout", function(d) {
             d3.select(this).style("stroke", "none")
@@ -131,22 +111,33 @@ d3.json(url, function(data) {
 
     var legendContainer = svg.append("g")
         .attr("id", "legend")
-        .attr("transform", "translate(10,0)")
+        .attr("x", 520)
 
 
-
-    var h = 0
+    var h = 525
+    var w = -500
     for (i in colors) {
-        legendContainer.append("rect")
+
+        var tempText = Math.round((data.baseTemperature + (colorScale.invertExtent(colors[i])[0])) * 10) / 10
+
+        legendContainer.append("rect", ".tick")
             .attr("x", width)
-            .attr("width", 25)
-            .attr("height", 25)
+            .attr("width", 40)
+            .attr("height", 30)
             .style("fill", colors[i])
-            .attr("transform", "translate(5," + h + ")")
-        h += 20
+            .style("stroke", 'black')
+            .style("stroke-width", 1.5)
+            .attr("id", "legendColor")
+            .attr("transform", `translate(${w} ,${h})`)
+
+        legendContainer.append("text")
+            .attr("x", width)
+            .text(tempText)
+            .attr("transform", `translate(${w} ,${h+50})`)
+
+        w += 40
 
     }
-
 
 
 })
