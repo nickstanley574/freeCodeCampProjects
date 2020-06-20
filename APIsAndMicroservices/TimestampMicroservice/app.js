cb922891a662da740e14a9c790bbc6e64c1fda49
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2020-06-20 14:13:35 -0500

    add __dirname to index path

--- APIsAndMicroservices/TimestampMicroservice/app.js
+++ APIsAndMicroservices/TimestampMicroservice/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile('index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 var listener = app.listen(process.env.PORT || 3000, function() {
