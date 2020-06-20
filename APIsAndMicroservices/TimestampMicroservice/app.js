7b3bbcffd1a748e23697f9c116a50f61602f6551
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2020-06-20 14:29:20 -0500

    handle 404 and 500 for timestampMS and addid timestamp api skeleton

--- APIsAndMicroservices/TimestampMicroservice/app.js
+++ APIsAndMicroservices/TimestampMicroservice/app.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const router = express.Router();
+
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+
+app.get('/api/timestamp', function(rq, res) {
+    return res.status(200).send({ success: 'true' });
+});
+
+
+// 404
+app.use(function(req, res, next) {
+    return res.status(404).send({ message: 'Route' + req.url + ' Not found.' });
+});
+
+
+// 500 - Any server error
+app.use(function(err, req, res, next) {
+    return res.status(500).send({ error: err });
+});
+
 
 var listener = app.listen(process.env.PORT || 3000, function() {
     console.log('Your app is listening on port ' + listener.address().port);
