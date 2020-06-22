7c0f23754846cca415af166c45191559682781b7
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2020-06-22 18:53:25 -0500

    init commit of RequestHeaderParser

---
+++ APIsAndMicroservices/RequestHeaderParser/README.md
@@ -0,0 +1,17 @@
+# API Project: Request Header Parser Microservice for freeCodeCamp
+
+FCC Github: https://github.com/freeCodeCamp/boilerplate-project-headerparser/
+
+Build a full stack JavaScript app that is functionally similar to this: https://dandelion-roar.glitch.me/.
+
+## User stories:
+
+- [ ] I can get the IP address, preferred languages (from header Accept-Language) and system infos (from header User-Agent) for my device.
+
+## Example usage:
+
+`[base_url]/api/whoami`
+
+## Example output:
+
+`{"ipaddress":"159.20.14.100","language":"en-US,en;q=0.5","software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}`
---
+++ APIsAndMicroservices/RequestHeaderParser/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// index.html
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// 404
+app.use(function(req, res, next) {
+    return res.status(404).send({ message: 'Route ' + req.url + ' Not found.' });
+});
+
+// 500 - Any server error
+app.use(function(err, req, res, next) {
+    return res.status(500).send({ error: err });
+});
+
+
+var listener = app.listen(process.env.PORT || 3000, function() {
+    console.log('Your app is listening on port ' + listener.address().port);
+});
