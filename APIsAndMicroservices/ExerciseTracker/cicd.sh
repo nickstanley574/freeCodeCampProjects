e20aa2eb2064e329af13c53cb70dfab610c8ee21
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2020-07-03 13:08:08 -0500

    cicd init and docker

--- APIsAndMicroservices/ExerciseTracker/cicd.sh
+++ APIsAndMicroservices/ExerciseTracker/cicd.sh
@@ -3,10 +3,21 @@ set -e
 
 echo -e "\n=== Build ===\n"
 
+docker build -t exercisetracker:latest .
+
 echo -e "\n=== Tests ===\n"
+
+echo "NO TESTS YET!!!"
+sleep 3
 
 echo -e "\n=== Push ===\n"
 
+docker tag exercisetracker registry.heroku.com/fcc-exercisetracker-ns574/web
+docker push registry.heroku.com/fcc-exercisetracker-ns574/web
+sleep 2
+
 echo -e "\n=== Release ===\n"
+
+heroku container:release web --app fcc-exercisetracker-ns574
 
 echo -e "Done."
