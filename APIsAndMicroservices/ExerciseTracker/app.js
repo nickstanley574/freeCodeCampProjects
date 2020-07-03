75b17ae1b58d0876a17df47c04ce3fd181dab9ae
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2020-07-03 12:19:40 -0500

    init database and username

--- APIsAndMicroservices/ExerciseTracker/app.js
+++ APIsAndMicroservices/ExerciseTracker/app.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 
 const cors = require('cors')
 app.use(cors())
 
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+
+// DATABASE CONNECTION
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track', { useNewUrlParser: true, useUnifiedTopology: true })
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// DATABASE SCHEMA
+
+const userSchema = new mongoose.Schema({
+    name: String
+
+});
+const User = mongoose.model("User", userSchema)
+
+
+// APP
 
 app.use(express.static('public'))
 
@@ -14,15 +36,61 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/exercise/new-user', function(req, res) {
+    console.log('/api/exercise/new-user')
+    let username = req.body.username
+    let newUser = new User({ name: username });
+    newUser.save((err, newUser) => {
+        if (err) {
+            res.json({ error: "Opps! Something is Wrong!" })
+            return console.error(err);
+        }
+        res.json({
+            name: newUser.name,
+            _id: newUser.id
+        })
+    })
+});
+
+// app.post('/api/exercise/add', function(req, res) {});
 
 
-// 404
-app.use((req, res, next) => {
-    res.status(404).json({ error: '404 - Not Found' });
+app.get('/is-mongoose-ok', function(req, res) {
+    console.log("/is-mongoose-ok")
+    if (mongoose) {
+        res.json({ isMongooseOk: !!mongoose.connection.readyState })
+    } else {
+        res.json({ isMongooseOk: false })
+    }
 });
 
 
-// 500
+// Not found middleware
+app.use((req, res, next) => {
+    return next({ status: 404, message: 'not found' })
+})
+
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+    let errCode, errMessage
+
+    if (err.errors) {
+        // mongoose validation error
+        errCode = 400 // bad request
+        const keys = Object.keys(err.errors)
+            // report the first validation error
+        errMessage = err.errors[keys[0]].message
+    } else {
+        // generic or custom error
+        errCode = err.status || 500
+        errMessage = err.message || 'Internal Server Error'
+    }
+    res.status(errCode).type('txt')
+        .send(errMessage)
+})
+
+
 var listener = app.listen(process.env.PORT || 3000, function() {
     console.log('Your app is listening on port ' + listener.address().port);
 })
