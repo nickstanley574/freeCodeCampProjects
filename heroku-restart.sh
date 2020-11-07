60b4fb61b0f24026df979a89b1944018d19cfe76
nickstanley574 <40447503+nickstanley574@users.noreply.github.com>
2020-11-07 15:43:00 -0600

    git history test

--- heroku-restart.sh
+++ heroku-restart.sh
@@ -2,7 +2,7 @@ echo "Restarting freecodecamp-projects-ns574 ..."
 echo
 heroku ps:restart --app freecodecamp-projects-ns574
 echo
-echo "local:  $(cat .git/refs/remotes/origin/master)"
+echo "local:  $(git rev-parse --verify HEAD)"
 echo "github: $(curl -s https://api.github.com/repos/nickstanley574/freeCodeCampProjects/git/refs/heads/master | jq -r .object.sha)"
 sleep 5
 echo "live:   $(curl -s https://freecodecamp-projects-ns574.herokuapp.com/sha)"
